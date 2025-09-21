ff5eb56ef5c1ae51dd79c5041348d0b723389b2e
pruebasdecodigos <pruebasparatd@gmail.com>
2025-09-21 07:18:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// SLIDER
 const slider = document.querySelector(".slider");
 const slides = document.querySelectorAll(".slider img");
 
@@ -7,11 +8,18 @@ const totalSlides = slides.length;
 function showNextSlide() {
   currentIndex++;
   if (currentIndex >= totalSlides) {
-    currentIndex = 0; // volver a la primera
+    currentIndex = 0;
   }
   const offset = -currentIndex * 100;
   slider.style.transform = `translateX(${offset}%)`;
 }
 
-// cambia cada 4 segundos
 setInterval(showNextSlide, 4000);
+
+// MENU HAMBURGUESA
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+});
